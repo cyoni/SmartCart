2437589ca4067e4052646b2e227db46877da1961
cyoni <cyoni10@gmail.com>
2019-12-24 00:32:18 +0200

    i

--- app/src/main/java/com/example/smartcart/MainActivity.java
+++ app/src/main/java/com/example/smartcart/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.smartcart;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.type.Money;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -82,15 +102,79 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void addSomething(final View view) {
-/*    *//*    Gson gson = new Gson();
-       userBoard user = gson.fromJson(getIntent().getStringExtra("userMetaData"), userBoard.class);
-        controller.toast(this, user.getAddress());*//*
-    controller.toast(this, _user.getAddress());*/
+
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("items").addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+
+                    Iterator<DataSnapshot> a = dataSnapshot.getChildren().iterator();
+                    System.out.println(dataSnapshot.getChildrenCount() + "= num");
+                    DataSnapshot tmp;
+
+                    while (a.hasNext()) {
+                        tmp = a.next();
+                        final HashMap<String, Object> dataMap = (HashMap<String, Object>) tmp.getValue();
+
+                        String n = dataMap.get("price")+"";
+                        String x = tmp.getKey() + "";
+                        System.out.println("name: " + x + ", price: " + n);
+
+
+                    }
+
+                }
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+
+/*
+        //Get datasnapshot at your "users" root node
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users");
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        collectPhoneNumbers((Map<String,Object>) dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });*/
+
+    }
+
+    private void collectPhoneNumbers(Map<String,Object> users) {
+
+
+        //iterate through each user, ignoring their UID
+        for (Map.Entry<String, Object> entry : users.entrySet()){
+            //Get user map
+
+          System.out.println( entry.getValue());
+
+
+            //Get phone field and append to list
+           // phoneNumbers.add(singleUser.get("email")+"");
+        }
+
+     ///   System.out.println(phoneNumbers.toString());
     }
 
     public void shopping(View view) {
-        Intent s = new Intent(this,ShoppingActivity.class);
-        startActivity(s);
+
     }
     public void lastShopping(View view)
     {
